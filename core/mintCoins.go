994e2d6d47a040906c28928fc14f471e633c62a6
JunBin <j.junbin@qq.com>
2023-03-31 17:49:20 +0800

    🐞 fix: small bugs

--- core/mintCoins.go
+++ core/mintCoins.go
@@ -1,10 +1,13 @@
 package core
 
-//TODO: for coinbase transaction
-//func NewCoinbaseTx() (t *Transaction) {
-	//return
-//}
-func NewCoinbaseTx()(address string, data string) (t *Transaction) {
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+)
+
+
+func NewCoinbaseTx(address string, data string) (t *Transaction) {
 	//characteristics of mint coin
 	//only have one input
 	//Do not need transaction ID
@@ -21,16 +24,16 @@ func NewCoinbaseTx()(address string, data string) (t *Transaction) {
 	}
 	//Coinbase Transaction: each block should contain a coinbase transaction at the very first place for transactions to mint 50 coins. 
 	//const COINBASE_AMOUNT: number = 50;
-	const reward = 50
+	// const reward = 50
 	//Input: the input of coinbase transaction is zero. 
 	//input := TXInput{[]byte{}, -1, nil, []byte(data)}
-	input := TxInput{ TXid:[]byte{}, index: -1, nil, []byte(data)}
+	input := TXInput{ []byte{}, -1, nil, []byte(data)}
 	//Output: the output consists of an address and the amount of minted coins. 
 	//output := NewTXOutput(subsidy, to)
-	output := Txoutput{ value: reward, PubKeyHash: address}
-	tx := Transaction{ TXID: []byte{}, TxInput: []TxInput{input}, Txoutput:[]Txoutput{output}}
+	output := NewTXOutput(subsidy, address)
+	tx := Transaction{ [32]byte{}, []TXInput{input}, []TXOutput{*output}}
 	//Transaction ID: the transaction ID of coinbase transaction is calculated by taking a hash of the transaction contents. 
-	tx.ID = tx.SetHash()
+	tx.ID = tx.Hash()
 
 	return &tx
 
